ea58a3e07fb6452cedc129784bffc91d5a91f435
Joe Santoru <joesantoru@gmail.com>
2017-08-16 21:24:26 -0400

    added sidenav, added input fields for espp and personal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,29 @@ import { NgModule } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {MdButtonModule, MdInputModule} from '@angular/material';
-import {MdCardModule} from '@angular/material';
+import {MdButtonModule, MdInputModule, MdCardModule, MdSidenavModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {TickerComponent} from './ticker/ticker.component';
 import { EsppComponent } from './espp/espp.component';
 import { PersonalComponent } from './personal/personal.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TickerComponent,
     EsppComponent,
-    PersonalComponent
+    PersonalComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdInputModule,
     MdCardModule,
-    MdButtonModule
+    MdButtonModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
