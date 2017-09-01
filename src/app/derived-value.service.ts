17a463eca7a5b1b43344320e87d6942f02170e56
Joe Santoru <joseph.santoru@thomsonreuters.com>
2017-09-01 14:26:12 -0400

    implemented a couple more

--- src/app/derived-value.service.ts
+++ src/app/derived-value.service.ts
@@ -10,23 +10,23 @@ export class DerivedValueService {
     let derivedValues : DerivedValues = new DerivedValues();
 
     // amount to contribute = salary * contrPercent / (12 / holding period)
-    derivedValues.amountToContribute = this.calculateAmountToContribute(inputEntity);
-    derivedValues.discountSharePrice = this.calculateDiscountSharePrice(inputEntity);
+    derivedValues.amountToContribute = this.calculateAmountToContribute(inputEntity, derivedValues);
+    derivedValues.discountSharePrice = this.calculateDiscountSharePrice(inputEntity, derivedValues);
 
-    derivedValues.numShares = this.calculateNumShares(inputEntity);
-    derivedValues.regularPurchase = this.calculateRegularPurchase(inputEntity);
-    derivedValues.discountPurchase = this.calculateDiscountPurchase(inputEntity);
+    derivedValues.numShares = this.calculateNumShares(inputEntity, derivedValues);
+    derivedValues.regularPurchase = this.calculateRegularPurchase(inputEntity, derivedValues);
+    derivedValues.discountPurchase = this.calculateDiscountPurchase(inputEntity, derivedValues);
 
-    derivedValues.discount = this.calculateDiscount(inputEntity);
-    derivedValues.taxesOnDiscount = this.calculateTaxesOnDiscount(inputEntity);
+    derivedValues.discount = this.calculateDiscount(inputEntity, derivedValues);
+    derivedValues.taxesOnDiscount = this.calculateTaxesOnDiscount(inputEntity, derivedValues);
 
-    derivedValues.sharePriceAtSell = this.calculateSharePriceAtSell(inputEntity);
-    derivedValues.sellAmount = this.calculateSellAmount(inputEntity);
-    derivedValues.earningsOverRegularPurchase = this.calculateEarningsOverRegularPurchase(inputEntity);
-    derivedValues.taxOnEarnings = this.calculateTaxOnEarnings(inputEntity);
+    derivedValues.sharePriceAtSell = this.calculateSharePriceAtSell(inputEntity, derivedValues);
+    derivedValues.sellAmount = this.calculateSellAmount(inputEntity, derivedValues);
+    derivedValues.earningsOverRegularPurchase = this.calculateEarningsOverRegularPurchase(inputEntity, derivedValues);
+    derivedValues.taxOnEarnings = this.calculateTaxOnEarnings(inputEntity, derivedValues);
 
-    derivedValues.dividendAmount = this.calculateDividendAmount(inputEntity);
-    derivedValues.taxOnDividend = this.calculateTaxOnDividend(inputEntity);
+    derivedValues.dividendAmount = this.calculateDividendAmount(inputEntity, derivedValues);
+    derivedValues.taxOnDividend = this.calculateTaxOnDividend(inputEntity, derivedValues);
 
     this.logDerivedValues(derivedValues);
 
@@ -38,56 +38,56 @@ export class DerivedValueService {
     console.log(derivedValues.discountSharePrice);
   }
 
-  calculateAmountToContribute(inputEntity: InputEntity) : number {
+  calculateAmountToContribute(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return inputEntity.personalInputEntity.salary * inputEntity.personalInputEntity.contributionPercent/100 /
       (12 / inputEntity.esppInputEntity.holdingPeriodMonths);
   }
 
-  calculateDiscountSharePrice(inputEntity: InputEntity) : number {
+  calculateDiscountSharePrice(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return inputEntity.companyInputEntity.value - (inputEntity.companyInputEntity.value * inputEntity.esppInputEntity.discount/100);
   }
 
-  calculateNumShares(inputEntity: InputEntity) : number {
+  calculateNumShares(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
+    return derivedValues.amountToContribute / derivedValues.discountSharePrice;
+  }
+
+  calculateRegularPurchase(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
+    return derivedValues.numShares * inputEntity.companyInputEntity.value;
+  }
+
+  calculateDiscountPurchase(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
+    return derivedValues.numShares * derivedValues.discountSharePrice;
+  }
+
+  calculateDiscount(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 
-  calculateRegularPurchase(inputEntity: InputEntity) : number {
+  calculateTaxesOnDiscount(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 
-  calculateDiscountPurchase(inputEntity: InputEntity) : number {
+  calculateSharePriceAtSell(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 
-  calculateDiscount(inputEntity: InputEntity) : number {
+  calculateSellAmount(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 
-  calculateTaxesOnDiscount(inputEntity: InputEntity) : number {
+  calculateEarningsOverRegularPurchase(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 
-  calculateSharePriceAtSell(inputEntity: InputEntity) : number {
+  calculateTaxOnEarnings(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 
-  calculateSellAmount(inputEntity: InputEntity) : number {
+  calculateDividendAmount(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 
-  calculateEarningsOverRegularPurchase(inputEntity: InputEntity) : number {
-    return 0;
-  }
-
-  calculateTaxOnEarnings(inputEntity: InputEntity) : number {
-    return 0;
-  }
-
-  calculateDividendAmount(inputEntity: InputEntity) : number {
-    return 0;
-  }
-
-  calculateTaxOnDividend(inputEntity: InputEntity) : number {
+  calculateTaxOnDividend(inputEntity: InputEntity, derivedValues: DerivedValues) : number {
     return 0;
   }
 }
