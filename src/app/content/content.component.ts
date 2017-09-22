462324b76275be6b3c292a1e9756090210239134
Joe Santoru <joseph.santoru@thomsonreuters.com>
2017-09-22 13:52:16 -0400

    fill table with actual calculated values

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {InputEntity} from "../input/input.component";
 import {DerivedValues} from "../derived-value.service";
+import {TableData} from "../app.component";
 
 @Component({
   selector: 'app-content',
@@ -11,6 +12,7 @@ export class ContentComponent implements OnInit {
 
   @Input() inputEntity: InputEntity;
   @Input() derivedValues: DerivedValues;
+  @Input() tableData: TableData;
 
   constructor() { }
 
