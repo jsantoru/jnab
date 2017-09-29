8962272e967e731cb1c6b6310eeb0a85b2bc6408
Joe Santoru <joseph.santoru@thomsonreuters.com>
2017-09-29 15:12:16 -0400

    removing commented code, cleaning up

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
@@ -10,69 +11,41 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/merge';
 
-const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
-  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
-  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
-  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
-  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+export class CompanySearchResult {
+  name: string;
+  ticker: string;
+}
 
 @Injectable()
 export class SearchService {
-
-  // TODO: move this and the ajax call to a service
   blah1: string = "67.240";
   blah2: string = "241.124";
   blah: string = this.blah1 + "." + this.blah2;
 
   constructor(private httpClient: HttpClient) {}
 
-  consoleLog(value) {
-    console.log(value);
-  }
+  executeCompanySearch(companySearchValue) : Observable<CompanySearchResult[]> {
+    console.log("company search value:", companySearchValue);
+    var url: string = 'http://' + this.blah + ':6001/api/stock/query?query=' + companySearchValue + "&apikey=demo";
 
-  onCompanySearchEnter(companySearchValue) {
-    console.log(companySearchValue);
+    // only execute the search if we have a search term
+    if (companySearchValue && companySearchValue.length > 2) {
+      return this.httpClient.get(url).map(
+        // data is an array of objects {"name":"Thomson Reuters", "ticker":"TRI"}
+        (data: CompanySearchResult[]) => {
+          console.log("Search Results:", data);
+          return data;
+        },
+        (err) => {
+          console.error("ERROR:", err);
+          // just return an empty array if there's an error
+          return [];
+        }
+      );
+    }
 
-    let url: string = 'http://' + this.blah + ':6001/api/stock/query?query=' + companySearchValue + "&apikey=demo";
-
-    /*
-     // get the current value of the ticker
-     this.httpClient.get(url).subscribe(
-     // currently the response data is JUST the value
-     (data) => {
-     console.log("RESPONSE:", data);
-     return data;
-     },
-     (err) => {
-     console.log("ERROR:", err);
-     //callback("ERROR:"+err, err);
-     }
-     );
-     */
-
-    // return the observable, don't yet process the data
-    return this.httpClient.get(url).map(
-      // data is a list of objects {"name":"Thomson Reuters", "ticker":"TRI"}
-      (data : any[]) => {
-        console.log("RESPONSE:", data);
-
-        let results: string[] = [];
-
-        data.forEach(item => {
-          console.log("result",item.name);
-          results.push(item.name);
-        });
-
-        return results;
-      },
-      (err) => {
-        console.log("ERROR:", err);
-        //callback("ERROR:"+err, err);
-      }
-    );
+    let emptyArray : CompanySearchResult[] = [];
+    return Observable.of(emptyArray);
   }
 }
 
@@ -84,10 +57,10 @@ export class SearchService {
 export class NavComponent implements OnInit {
   title = 'ESPP Calculator';
 
-  model: any;
+  companyName: any;
 
-  constructor(private searchService : SearchService) {
-    // this is to avoid it being unavailable in the observable below
+  constructor(private searchService: SearchService) {
+    // this is to avoid searchService being unavailable in the observable below
     // https://www.bountysource.com/issues/38514817-typeahead-this-is-undefined
     this.search = this.search.bind(this);
   }
@@ -95,43 +68,35 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
-  search(text$: Observable<string>) {
-
-    text$.subscribe(
-      (data) => {
-        console.log("data", data);
-      }
-    );
-
+  search(text$: Observable<string>): Observable<string[]> {
     return text$
       // wait 200 ms before getting different values
       .debounceTime(200)
       // only emit when the current value is different than the last
       .distinctUntilChanged()
       .do(() => console.log("searching"))
-      // process the search term
-      .switchMap(term => {
+      // process the search term and execute the search
+      // and switch to the company search response observable
+      .switchMap(term =>
         // can we make the ajax call here? need a callback?
-        return this.searchService.onCompanySearchEnter(term);
-        /*
-        // if it's too short, don't return anything yet
-        if( term.length < 2 ) {
-          return [];
-        }
-        else {
-          // filter down to the states that match the search
-          let results = states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1);
-          return results.slice(0, 10); // return the first 10
-        }
-        */
-      })
-      .do(() => console.log("done searching"));
+        this.searchService.executeCompanySearch(term)
+      )
+      // process the company search results
+      .map(searchResults => {
+        let companyNames: string[] = [];
+
+        searchResults.forEach(companySearchResult => {
+          companyNames.push(this.buildCompanySearchResultString(companySearchResult));
+        });
+
+        // return the first 10 company names
+        // this is what the ui component needs
+        return companyNames.slice(0, 10);
+      });
   };
 
-  searchHelper(text$: Observable<string>) {
-
+  buildCompanySearchResultString(companySearchResult: CompanySearchResult) {
+    let tickerString = companySearchResult.ticker ? " (" + companySearchResult.ticker + ")": "";
+    return companySearchResult.name + tickerString;
   }
-
-
-
 }
